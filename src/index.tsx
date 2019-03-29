852db0f26cd8c8bf6b2995e2df2cd9d7efc5893d
William Luke <william@ordino.ai>
2019-03-29 13:30:25 +0300

    Clean

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import { MasterLoboloRack1, MasterLoboloRack2, MasterLoboloRack3 } from './figmaComponents';
+const Page: React.SFC = () => {
+  return(
+    <div>
+      <MasterLoboloRack1/>
+      <MasterLoboloRack2/>
+      <MasterLoboloRack3/>
+    </div>
+  )
+}
+ReactDOM.render(<Page/>,document.body);
+registerServiceWorker();
